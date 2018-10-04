5311b7429762a1af53c7ec943e8f7c1b8c844b55
Nitin Vinayak Agrawal <f20170415@gmail.com>
2018-10-04 13:51:11 +0530

    models.py complete

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,9 +5,37 @@ from django.core.urlresolvers import reverse
 
 class Post(models.Model):
     author=models.ForeignKey("auth.User")
-    title=models.Charfield(max_length=200)
+    title=models.CharField(max_length=200)
     text=models.TextField()
     create_date=models.DateTimeField(default=timezone.now())
     published_date=models.DateTimeField(blank=True,null=False)
 
-    
+    def publish(self):
+        self.published_date=timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail",kwargs={'pk':self.pk})
+
+class Comment(models.Model):
+    post=models.ForeignKey('blog.Post',related_name='comments')
+    author=models.CharField(max_length=200)
+    text=models.TextField()
+    create_date=models.DateTimeField(default=timezone.now())
+    approved_comment=models.BooleanField(default=False)
+
+    def approve_comments(self):
+        self.approved_comment=True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+    def get_absolute_url(self):
+        return reverse('post_list')
